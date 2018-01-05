a0312d5231ad550a3c64d0c03806b2c8edf891ac
zhandao <x@skippingcat.com>
2018-01-05 20:12:11 +0800

    fix lth

--- lib/oas_objs/schema_obj.rb
+++ lib/oas_objs/schema_obj.rb
@@ -61,9 +61,9 @@ module OpenApi
         if (lth = _length || '').is_a?(Array)
           min, max = [lth.first&.to_i, lth.last&.to_i]
         elsif lth['ge']
-          max = lth.to_s.split('_').last.to_i
-        elsif lth['le']
           min = lth.to_s.split('_').last.to_i
+        elsif lth['le']
+          max = lth.to_s.split('_').last.to_i
         end
 
         if processed[:type] == 'array'
@@ -92,12 +92,10 @@ module OpenApi
 
       def pattern_default_and_other
         {
-            **{
-                pattern:    _pattern.is_a?(String)? _pattern : _pattern&.inspect&.delete('/'),
-                default:    _default,
-                examples:   self[:examples].present? ? ExampleObj.new(self[:examples], self[:exp_by]).process : nil
-            },
-            **{ as: _as, permit: _permit, not_permit: _npermit, req_if: _req_if, opt_if: _opt_if, blankable: _blank }
+            pattern:    _pattern.is_a?(String)? _pattern : _pattern&.inspect&.delete('/'),
+            default:    _default,
+            examples:   self[:examples].present? ? ExampleObj.new(self[:examples], self[:exp_by]).process : nil,
+            as: _as, permit: _permit, not_permit: _npermit, req_if: _req_if, opt_if: _opt_if, blankable: _blank
         }
       end
 
