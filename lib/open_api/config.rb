7fb2952f2d1d0df80e18815ac66d337ae7a997a5
zhandao <x@skippingcat.com>
2019-02-12 19:21:39 +0800

    C / Set cattr_accessor default value by using block

--- lib/open_api/config.rb
+++ lib/open_api/config.rb
@@ -7,12 +7,12 @@ module OpenApi
   module Config
     include ConfigDSL
 
-    cattr_accessor :default_run_dry, default: false
+    cattr_accessor(:default_run_dry) { false }
 
     # [REQUIRED] The location where .json doc file will be output.
-    cattr_accessor :file_output_path, default: 'public/open_api'
+    cattr_accessor(:file_output_path) { 'public/open_api' }
 
-    cattr_accessor :doc_location, default: ['./app/**/*_doc.rb']
+    cattr_accessor(:doc_location) { ['./app/**/*_doc.rb'] }
 
     cattr_accessor :rails_routes_file
 
@@ -20,9 +20,9 @@ module OpenApi
 
     # Everything about OAS3 is on https://github.com/OAI/OpenAPI-Specification/blob/OpenAPI.next/versions/3.0.0.md
     # Getting started: https://swagger.io/docs/specification/basic-structure/
-    cattr_accessor :open_api_docs, default: { }
+    cattr_accessor(:open_api_docs) { { } }
 
-    cattr_accessor :file_format, default: 'binary'
+    cattr_accessor(:file_format) { 'binary' }
 
     def self.docs
       open_api_docs
